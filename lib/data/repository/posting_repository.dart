e88aaee4d195f1028989347322a2f0e38ea0ec6b
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-06 04:33:02 +0700

    CREATE - Admin Posting Repository Add Post Burung

---
+++ lib/data/repository/posting_repository.dart
@@ -0,0 +1,38 @@
+
+import 'dart:convert';
+import 'dart:developer';
+import 'package:canary_app/data/model/request/admin/posting_jual_request_model.dart';
+import 'package:canary_app/data/model/response/burung_semua_tersedia_model.dart';
+import 'package:canary_app/service/service_http_client.dart';
+import 'package:dartz/dartz.dart';
+
+class PostingRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  PostingRepository(this. _serviceHttpClient);
+
+
+  Future<Either<String, BurungSemuaTersediabyIdModel>>addPostBurung(
+    PostingJualRequestModel requestModel,
+  )async{
+    try{
+      final response = await _serviceHttpClient.postWithToken(
+        'admin/posting-jual',
+        requestModel.toJson(),
+      );
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 201){
+        final profileResponse = BurungSemuaTersediabyIdModel.fromJson(
+          jsonResponse,
+        );
+        log("Add Burung successful: ${profileResponse.message}");
+        return Right(profileResponse);
+      }else{
+        log("Add Burung failed: ${jsonResponse['message']}");
+        return Left(jsonResponse['message'] ?? "Ad burung failed");
+      }
+    }catch(e){
+      log("Error in add burung : $e");
+      return Left("An error occurred while post burung: $e");
+    }
+  }
+}
