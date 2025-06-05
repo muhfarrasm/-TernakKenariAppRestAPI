5de83ddcc63c02c26716815bd2c80a3cfb991b17
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-05 17:19:51 +0700

    Repair

--- lib/data/model/request/admin/admin_profile_request.dart
+++ lib/data/model/request/admin/admin_profile_request.dart
@@ -1,14 +1,10 @@
 import 'dart:convert';
 
 class AdminProfileRequest {
-    final String? message;
-    final int? statusCode;
-    final Data? data;
+    final String? name;
 
     AdminProfileRequest({
-        this.message,
-        this.statusCode,
-        this.data,
+        this.name,
     });
 
     factory AdminProfileRequest.fromJson(String str) => AdminProfileRequest.fromMap(json.decode(str));
@@ -16,38 +12,10 @@ class AdminProfileRequest {
     String toJson() => json.encode(toMap());
 
     factory AdminProfileRequest.fromMap(Map<String, dynamic> json) => AdminProfileRequest(
-        message: json["message"],
-        statusCode: json["status_code"],
-        data: json["data"] == null ? null : Data.fromMap(json["data"]),
-    );
-
-    Map<String, dynamic> toMap() => {
-        "message": message,
-        "status_code": statusCode,
-        "data": data?.toMap(),
-    };
-}
-
-class Data {
-    final int? id;
-    final String? name;
-
-    Data({
-        this.id,
-        this.name,
-    });
-
-    factory Data.fromJson(String str) => Data.fromMap(json.decode(str));
-
-    String toJson() => json.encode(toMap());
-
-    factory Data.fromMap(Map<String, dynamic> json) => Data(
-        id: json["id"],
         name: json["name"],
     );
 
     Map<String, dynamic> toMap() => {
-        "id": id,
         "name": name,
     };
 }
