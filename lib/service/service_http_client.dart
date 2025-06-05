771783ada83e2f02e412c4cc4e4500e65d2e7ebc
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-05 19:48:51 +0700

    REPAIR - service http client mengganti api nya

--- lib/service/service_http_client.dart
+++ lib/service/service_http_client.dart
@@ -4,7 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class ServiceHttpClient {
-  final String baseUrl = 'http://10.0.0.2:8000:/api/';
+  final String baseUrl = 'http://10.0.0.2:8000/api/';
   final secureStorage =  FlutterSecureStorage();
   
   //post
