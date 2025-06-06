0109e0459016e455e842ddada74bd8f203516e4e
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-06 07:59:43 +0700

    CREATE - Login Screean deklarasi variable

---
+++ lib/data/model/request/auth/login_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+  late final GlobalKey<FormState>;
+  bool isShowPassword = false;
+  
+}
