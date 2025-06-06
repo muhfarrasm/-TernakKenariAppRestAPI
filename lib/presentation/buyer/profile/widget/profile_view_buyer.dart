6f9fc237ad878d63824cf69231fa2cb49fad27d3
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-06 18:47:02 +0700

    CREATE - form profile buyer

---
+++ lib/presentation/buyer/profile/widget/profile_view_buyer.dart
@@ -0,0 +1,57 @@
+import 'package:canary_app/core/extensions/extensions.dart';
+import 'package:flutter/material.dart';
+import 'package:canary_app/data/model/response/buyer/buyer_profile_response_model.dart';
+
+
+class ProfileViewBuyer extends StatelessWidget {
+  final Data profile;
+  const ProfileViewBuyer({super.key, required this.profile});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Nama: ${profile.name}",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.05,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            Text(
+              "Nama: ${profile.address}",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            Text(
+              "Nama: ${profile.phone}",
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.04,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                context.pushAndRemoveUntil(
+                  const BuyerHomeScreen(),
+                  (route) => false,
+                );
+              },
+              child: const Text("Go to Home"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
