94bc1a2dc99c821342172156c88f0b3ab6581ef3
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-06 07:13:16 +0700

    CREATE - login Bloc

---
+++ lib/presentation/auth/bloc/login_bloc.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:canary_app/data/model/request/auth/login_request_model.dart';
+import 'package:canary_app/data/model/response/auth_response_model.dart';
+import 'package:canary_app/data/repository/auth_repository.dart';
+import 'package:meta/meta.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final AuthRepository authRepository;
+
+
+  LoginBloc({required this.authRepository}) : super(LoginInitial()) {
+    on<LoginRequested>(_onLoginRequested);
+  
+  }
+
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<LoginState>emit,
+  )async{
+    emit(LoginLoading());
+
+    final result = await authRepository.login(event.requestModel);
+
+    result.fold(
+      (l) => emit(LoginFailure(error: l.message)),
+      (r) => emit(LoginSuccess(responseModel: r))
+      
+    );
+  }
+}
