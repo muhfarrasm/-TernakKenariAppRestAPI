5155fcbd545c2ffb50f59ecf4adb963b3e57d166
muhfarrasm <muhammadfarrasm@gmail.com>
2025-06-06 08:28:59 +0700

    CREATE Text Field

--- lib/data/model/request/auth/login_screen.dart
+++ lib/data/model/request/auth/login_screen.dart
@@ -1,3 +1,5 @@
+import 'package:canary_app/core/components/custom_text_field.dart';
+import 'package:canary_app/core/components/spaces.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -32,6 +34,38 @@ class _LoginScreenState extends State<LoginScreen> {
   
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SpaceHeight(170),
+                Text(
+                  "SELAMAT DATANG KEMBALI",
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                  const SpaceHeight(30),
+                  CustomTextField(
+                    validator:'email tidak boleh kosong',
+                    controller: emailController,
+                    label: 'Email',
+                    prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Icon(Icons.email),
+                  ),
+                  ),
+              ],
+            ),
+            )
+        )
+      )
+    );
   }
 }
